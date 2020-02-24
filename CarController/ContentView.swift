aecd9fb82b438d00199c54616f82dc0f1078361f
HackingGate <i@hackinggate.com>
2020-02-24 22:32:54 +0900

    Create Project

---
+++ CarController/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarController
+//
+//  Created by HG on 2020/02/24.
+//  Copyright © 2020 HG. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
