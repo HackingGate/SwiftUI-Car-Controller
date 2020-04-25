f300b357e13a376d449daf37e419a3bc07c24436
HackingGate <i@hackinggate.com>
2020-04-26 00:41:38 +0900

    Support all input values

--- CarController/ContentView.swift
+++ CarController/ContentView.swift
@@ -9,16 +9,90 @@
 import SwiftUI
 
 struct ContentView: View {
-    @ObservedObject var viewModel = ContentViewModel(controller: nil)
+    @ObservedObject var viewModel = ContentViewModel()
     var body: some View {
         VStack {
-            Text("\(viewModel.venderName ?? "No Controller Connected")")
-            Text("Left: \(viewModel.left.isPressed ? "Pressed" : "No")")
-            Text("Right: \(viewModel.right.isPressed ? "Pressed" : "No")")
-            Text("Up: \(viewModel.up.isPressed ? "Pressed" : "No")")
-            Text("Down: \(viewModel.down.isPressed ? "Pressed" : "No")")
-            Text("xAxis: \(viewModel.xAxis.value)")
-            Text("yAxis: \(viewModel.yAxis.value)")
+            VStack {
+                if (viewModel.venderName != nil) {
+                    Text("\(viewModel.venderName!)")
+                }
+                Text("\(viewModel.connected ? "Connected" : "Not Connected")")
+            }
+            .padding()
+            HStack {
+                VStack {
+                    Text("L1: \(viewModel.leftShoulder.isPressed ? "Pressed" : "No")")
+                    Text("L2: \(viewModel.leftTrigger.value)")
+                    if (viewModel.leftThumbstickButton != nil) {
+                        Text("L3: \(viewModel.leftThumbstickButton!.isPressed ? "Pressed" : "No")")
+                    } else {
+                        Text("L3: Not Activated")
+                    }
+                }
+                .padding()
+                Spacer()
+                VStack {
+                    Text("R1: \(viewModel.rightShoulder.isPressed ? "Pressed" : "No")")
+                    Text("R2: \(viewModel.rightTrigger.value)")
+                    if (viewModel.rightThumbstickButton != nil) {
+                        Text("R3: \(viewModel.rightThumbstickButton!.isPressed ? "Pressed" : "No")")
+                    } else {
+                        Text("R3: Not Activated")
+                    }
+                }
+                .padding()
+            }
+            Spacer()
+            HStack {
+                VStack {
+                    Text("Left: \(viewModel.dpad.left.isPressed ? "Pressed" : "No")")
+                    Text("Right: \(viewModel.dpad.right.isPressed ? "Pressed" : "No")")
+                    Text("Up: \(viewModel.dpad.up.isPressed ? "Pressed" : "No")")
+                    Text("Down: \(viewModel.dpad.down.isPressed ? "Pressed" : "No")")
+//                    Text("xAxis: \(viewModel.dpad.xAxis.value)")
+//                    Text("yAxis: \(viewModel.dpad.yAxis.value)")
+                }
+                .padding()
+                Spacer()
+                VStack {
+                    Text("A: \(viewModel.buttonA.isPressed ? "Pressed" : "No")")
+                    Text("B: \(viewModel.buttonB.isPressed ? "Pressed" : "No")")
+                    Text("X: \(viewModel.buttonX.isPressed ? "Pressed" : "No")")
+                    Text("Y: \(viewModel.buttonY.isPressed ? "Pressed" : "No")")
+                }
+                .padding()
+            }
+            Spacer()
+            HStack {
+                VStack {
+                    Text("Left: \(viewModel.leftThumbstick.left.isPressed ? "Pressed" : "No")")
+                    Text("Right: \(viewModel.leftThumbstick.right.isPressed ? "Pressed" : "No")")
+                    Text("Up: \(viewModel.leftThumbstick.up.isPressed ? "Pressed" : "No")")
+                    Text("Down: \(viewModel.leftThumbstick.down.isPressed ? "Pressed" : "No")")
+                    Text("xAxis: \(viewModel.leftThumbstick.xAxis.value)")
+                    Text("yAxis: \(viewModel.leftThumbstick.yAxis.value)")
+                }
+                .padding()
+                Spacer()
+                VStack {
+                    Text("Left: \(viewModel.rightThumbstick.left.isPressed ? "Pressed" : "No")")
+                    Text("Right: \(viewModel.rightThumbstick.right.isPressed ? "Pressed" : "No")")
+                    Text("Up: \(viewModel.rightThumbstick.up.isPressed ? "Pressed" : "No")")
+                    Text("Down: \(viewModel.rightThumbstick.down.isPressed ? "Pressed" : "No")")
+                    Text("xAxis: \(viewModel.rightThumbstick.xAxis.value)")
+                    Text("yAxis: \(viewModel.rightThumbstick.yAxis.value)")
+                }
+                .padding()
+            }
+//            Spacer()
+//            VStack {
+//                Text("Left: \(viewModel.left.isPressed ? "Pressed" : "No")")
+//                Text("Right: \(viewModel.right.isPressed ? "Pressed" : "No")")
+//                Text("Up: \(viewModel.up.isPressed ? "Pressed" : "No")")
+//                Text("Down: \(viewModel.down.isPressed ? "Pressed" : "No")")
+//                Text("xAxis: \(viewModel.xAxis.value)")
+//                Text("yAxis: \(viewModel.yAxis.value)")
+//            }
         }
     }
 }
--- CarController/ContentViewModel.swift
+++ CarController/ContentViewModel.swift
@@ -11,24 +11,46 @@ import GameController
 import Combine
 
 class ContentViewModel: ObservableObject {
-    @Published var controller: GCController?
+    // Controller status
+    @Published var connected = false
     @Published var venderName: String?
-    @Published var exGamepad: GCExtendedGamepad?
 
-    // Button
+    // 4 Face buttons
+    @Published var buttonA = GCControllerButtonInput() // Cross Button
+    @Published var buttonB = GCControllerButtonInput() // Circle Button
+    @Published var buttonX = GCControllerButtonInput() // Square Button
+    @Published var buttonY = GCControllerButtonInput() // Triangle Button
+
+    // Two Shoulders
+    @Published var leftShoulder = GCControllerButtonInput() // L1
+    @Published var rightShoulder = GCControllerButtonInput() // R1
+
+    // Two Triggers
+    @Published var leftTrigger = GCControllerButtonInput() // L2
+    @Published var rightTrigger = GCControllerButtonInput() // R2
+
+    // 2 Thumbstick buttons
+    @Published var leftThumbstickButton: GCControllerButtonInput? // L3
+    @Published var rightThumbstickButton: GCControllerButtonInput? // R3
+
+    // Directional Pad and 2 Thumbsticks
+    @Published var dpad = GCControllerDirectionPad()
+    @Published var leftThumbstick = GCControllerDirectionPad()
+    @Published var rightThumbstick = GCControllerDirectionPad()
+
+    // Button for all pads and thumbsticks
     @Published var left = GCControllerButtonInput()
     @Published var right = GCControllerButtonInput()
     @Published var up = GCControllerButtonInput()
     @Published var down = GCControllerButtonInput()
-    
-    // Axis
+
+    // Axis for all pads and thumbsticks
     @Published var xAxis = GCControllerAxisInput()
     @Published var yAxis = GCControllerAxisInput()
 
     private var disposables = Set<AnyCancellable>()
 
-    init(controller: GCController?) {
-        self.controller = controller
+    init() {
         self.subscribe()
     }
 
@@ -43,10 +65,19 @@ class ContentViewModel: ObservableObject {
             .publisher(for: Notification.Name.GCControllerDidConnect)
             .map( { ($0.object as! GCController) } )
             .sink(receiveValue: { controller in
-                print(controller)
-                self.controller = controller
+                self.connected = true
                 self.venderName = controller.vendorName
-                self.exGamepad = controller.extendedGamepad
+                if let exGamepad = controller.extendedGamepad {
+                    self.handleControllerValueChange(exGamepad)
+                }
+            })
+            .store(in: &disposables)
+        NotificationCenter.default
+            .publisher(for: Notification.Name.GCControllerDidDisconnect)
+            .map( { ($0.object as! GCController) } )
+            .sink(receiveValue: { controller in
+                self.connected = false
+                self.venderName = controller.vendorName
                 if let exGamepad = controller.extendedGamepad {
                     self.handleControllerValueChange(exGamepad)
                 }
@@ -57,17 +88,53 @@ class ContentViewModel: ObservableObject {
     func handleControllerValueChange(_ exGamepad: GCExtendedGamepad) {
         exGamepad.valueChangedHandler = {
             (gamepad: GCExtendedGamepad, element:GCControllerElement) in
+            // 4 Face buttons
+            if (gamepad.buttonA == element) {
+                self.buttonA = gamepad.buttonA
+            }
+            if (gamepad.buttonB == element) {
+                self.buttonB = gamepad.buttonB
+            }
+            if (gamepad.buttonX == element) {
+                self.buttonX = gamepad.buttonX
+            }
+            if (gamepad.buttonY == element) {
+                self.buttonY = gamepad.buttonY
+            }
+            // Two Shoulders
+            if (gamepad.leftShoulder == element) {
+                self.leftShoulder = gamepad.leftShoulder
+            }
+            if (gamepad.rightShoulder == element) {
+                self.rightShoulder = gamepad.rightShoulder
+            }
+            // Two Triggers
+            if (gamepad.leftTrigger == element) {
+                self.leftTrigger = gamepad.leftTrigger
+            }
+            if (gamepad.rightTrigger == element) {
+                self.rightTrigger = gamepad.rightTrigger
+            }
+            // 2 Thumbstick buttons
+            if (gamepad.leftThumbstickButton == element) {
+                self.leftThumbstickButton = gamepad.leftThumbstickButton
+            }
+            if (gamepad.rightThumbstickButton == element) {
+                self.rightThumbstickButton = gamepad.rightThumbstickButton
+            }
+            // Directional Pad
+            if (gamepad.dpad == element) {
+                self.dpad = gamepad.dpad
+                self.updateDirection(gamepad.dpad)
+            }
+            // 2 Thumbsticks
             if (gamepad.leftThumbstick == element) {
+                self.leftThumbstick = gamepad.leftThumbstick
                 self.updateDirection(gamepad.leftThumbstick)
-                self.updateAxis(gamepad.leftThumbstick)
             }
             if (gamepad.rightThumbstick == element) {
+                self.rightThumbstick = gamepad.rightThumbstick
                 self.updateDirection(gamepad.rightThumbstick)
-                self.updateAxis(gamepad.rightThumbstick)
-            }
-            if (gamepad.dpad == element) {
-                self.updateDirection(gamepad.dpad)
-                self.updateAxis(gamepad.dpad)
             }
         }
     }
@@ -77,9 +144,6 @@ class ContentViewModel: ObservableObject {
         self.right = pad.right
         self.up = pad.up
         self.down = pad.down
-    }
-    
-    func updateAxis(_ pad: GCControllerDirectionPad) {
         self.xAxis = pad.xAxis
         self.yAxis = pad.yAxis
     }
